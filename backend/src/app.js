d7704c7bf3cd2444bea5d7c1ceb25a08e542cc4b
jaswanth4237 <24p35a4237@acet.ac.in>
2026-05-06 22:26:21 +0530

    Update app setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const routs = require('./routes/feedbackRouters');
+
 //dabase connection
 const getConnection  = require('./config/db');
 const app = express();
@@ -14,10 +16,15 @@ app.get('/', (req,res)=>{
     res.send('Backend is running !');
 })
 
+app.use('/api', routs);
+
+app.get('/hi', (req,res)=>{
+    res.send('Backend is running  hi!');
+})
+
 //Start the server
 app.listen(7100, "0.0.0.0",()=>{
     console.log('Server is running on port 7100');
     console.log('http://localhost:7100');
     getConnection();
-    // console.log('Connected to MongoDB');
 } ) 
